36a2d47db731014893721ed5c685ba0f6ebff3c9
hosenA51 <hosenahmed2552@gmail.com>
2024-11-01 20:15:23 +0600

    save mark as read to some where

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { addToStoredReadList, addToStoredWishList } from '../../utility/addToDb';
 
 const BookDetail = () => {
 
@@ -11,6 +12,31 @@ const BookDetail = () => {
     const book = data.find(book => book.bookId === id);
 
     const { bookId: currentBookId, image, tags, bookName, author, category, totalPages, rating, review, publisher, yearOfPublishing } = book;
+
+    const handleRead = (id) => {
+        /**
+         * 1. Understand what to store or save: => bookId
+         * 2. where to store: database
+         * 3. array, list, collection: 
+         * 4. check: if the book is already in the readList.
+         * 5. if not, then add the book to the list
+         * 6. if yes, do not add the book
+         */
+
+        addToStoredReadList(id);
+    }
+    const handleWishList = (id) => {
+        /**
+         * 1. Understand what to store or save: => bookId
+         * 2. where to store: database
+         * 3. array, list, collection: 
+         * 4. check: if the book is already in the readList.
+         * 5. if not, then add the book to the list
+         * 6. if yes, do not add the book
+         */
+
+        addToStoredWishList(id);
+    }
 
     return (
         <div className="card lg:card-side flex my-20">
@@ -53,8 +79,8 @@ const BookDetail = () => {
                     </div>
                 </div>
                 <div className='flex gap-4'>
-                    <button className="btn btn-outline btn-accent font-semibold text-black px-4">Read</button>
-                    <button className="btn bg-[#50B1C9] font-semibold text-white px-6">Wishlist</button>
+                    <button onClick={()=> handleRead(bookId)} className="btn btn-outline btn-accent font-semibold text-black px-4">Read</button>
+                    <button onClick={()=> handleWishList(bookId)} className="btn bg-[#50B1C9] font-semibold text-white px-6">Wishlist</button>
                 </div>
             </div>
         </div>
